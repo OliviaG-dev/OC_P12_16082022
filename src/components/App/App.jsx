2070bf2cd5ad02c3b680d9fc2aeaccbe6c3f479b
Olivia Gautheron <oxtramag@hotmail.fr>
2022-08-19 16:11:26 +0200

    feat:add header dashboard and components keyFigure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,16 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from '../../pages/dashboard/dashboard';
 import Home from '../../pages/home/home';
+import Header from "../../components/header/header"
+import VerticalBar from "../../components/verticalBar/verticalBar"
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
+        <VerticalBar />
 				<Routes>
 					<Route path='/' element={<Home/>} />
 					<Route path='/user/:id' element={<Dashboard/>} />
---
+++ src/components/keyFigures/keyFigures.jsx
@@ -0,0 +1,55 @@
+import './keyFigures.css';
+import lipIcon from '../../assets/lipIcon.svg';
+import gluIcon  from'../../assets/gluIcon.svg';
+import proIcon from '../../assets/protIcon.svg';
+import calIcon from '../../assets/calIcon.svg';
+
+
+const KeyFigures = (icon) => {
+    let formatedProps = [
+        {
+            img: calIcon,
+            unit: "kCal",
+            label: "Calories",
+            data: icon.keyData.calorieCount,
+        },
+        {
+            img: proIcon,
+            unit: "g",
+            label: "Proteines",
+            data: icon.keyData.proteinCount,
+        },
+        {
+            img: gluIcon,
+            unit: "g",
+            label: "Glucides",
+            data: icon.keyData.carbohydrateCount,
+        },
+        {
+            img: lipIcon,
+            unit: "g",
+            label: "Lipides",
+            data: icon.keyData.lipidCount,
+        },
+    ];
+    return (
+        <div className='keyFigure'>
+            {formatedProps.map((item) => {
+                return (
+                    <div key={item.label}>
+                        <img className='keyFigure__img' src={item.img} alt={item.label} />
+                        <div className='keyFigure__name'>
+                            <span className='keyFigure__name__data'>
+                                {item.data}
+                                {item.unit}
+                            </span>
+                            <p className='keyFigure__name__label'>{item.label}</p>
+                        </div>
+                    </div>                    
+                )
+            })}
+        </div>
+    )
+}
+
+export default KeyFigures;
