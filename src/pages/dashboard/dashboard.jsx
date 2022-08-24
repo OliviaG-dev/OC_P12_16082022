a04f1f1938b3e5879ccad99f11b66a298ff1234f
Olivia Gautheron <oxtramag@hotmail.fr>
2022-08-25 00:26:08 +0200

    feat(data):add fetch data

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,38 +1,72 @@
-//import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import "./dashboard.css"
-import mockData from '../../services/mockData'
-import KeyFigures from '../../components/keyFigures/keyFigures';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./dashboard.css";
+import mockData from "../../services/mockData";
+import KeyFigures from "../../components/keyFigures/keyFigures";
+import { getUserInfo } from "../../services/api";
 //import PropTypes from 'prop-types';
 
-
 const Dashboard = () => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isMockData, setIsMockData] = useState(true);
+  const [data, setData] = useState([]);
 
-    let { id } = useParams();
-    let userId = parseInt(id);
+  let { id } = useParams();
+  let userId = parseInt(id);
 
-    const userInfo = mockData.USER_MAIN_DATA.find((item) => item.id === userId );
+  const userInfo = mockData.USER_MAIN_DATA.find((item) => item.id === userId);
 
+  useEffect(() => {
+    setLoading(true);
 
-    console.log(userInfo);
+    const getDataUser = async () => {
+      try {
+        const request = await getUserInfo(id);
+        setData(request.data);
+        setIsMockData(false);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getDataUser();
+  }, [id]);
 
-    return (
-        <>
-            <div className='container__header'>
-            <h1 className='header__title'>
-                Bonjour {''}
-                <span className='header__title--color'>{userInfo.userInfos.firstName}</span>
-            </h1>
-            <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-            </div>
-            
-            <div className='dashboard'>
-                <aside className='dashboard__keyFigures'>
-                    <KeyFigures {...userInfo} />
-                </aside>
-            </div>
-        </>
-    );
+  if (loading) {
+    return <>Loading...</>;
+  }
+
+  if (error) {
+    console.log(error);
+  }
+
+  return (
+    <>
+      <div className="container__header">
+        <h1 className="header__title">
+          Bonjour {""}
+          <span className="header__title--color">
+            {isMockData
+              ? userInfo?.userInfos?.firstName
+              : data.userInfos?.firstName}
+          </span>
+        </h1>
+        <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+      </div>
+
+      <div className="dashboard">
+        <aside className="dashboard__keyFigures">
+          {isMockData ? (
+            <KeyFigures {...userInfo} />
+          ) : (
+            <KeyFigures {...data} />
+          )}
+        </aside>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,43 +1,39 @@
-
 const getUserInfo = async (id) => {
-        try {
-            const response = await fetch(`http://localhost:3000/user/${id}`)
-            return response.data;
-        } catch (error) {
-            console.log(error);
-        }
-}
+  const response = await fetch(`http://localhost:3000/user/${id}`);
+  const data = await response.json();
+  return data;
+};
 
 const getUserPerformance = async (id) => {
-    try {
-        const response = await fetch(`http://localhost:3000/user/${id}/performance`)
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const response = await fetch(
+      `http://localhost:3000/user/${id}/performance`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getUserActivity = async (id) => {
-    try {
-        const response = await fetch(`http://localhost:3000/user/${id}/activity`)
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const response = await fetch(`http://localhost:3000/user/${id}/activity`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getUserAverage = async (id) => {
-    try {
-        const response = await fetch(`http://localhost:3000/user/${id}/average`)
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-export {
-    getUserInfo,
-    getUserPerformance,
-    getUserActivity,
-    getUserAverage
+  try {
+    const response = await fetch(`http://localhost:3000/user/${id}/average`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+export { getUserInfo, getUserPerformance, getUserActivity, getUserAverage };
