7440a3a5285a26833f4c57fc95768527787251ac
Olivia Gautheron <oxtramag@hotmail.fr>
2022-08-26 11:00:58 +0200

    feat(dataActivity):add dataActivity

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -5,35 +5,23 @@ const getUserInfo = async (id) => {
 };
 
 const getUserPerformance = async (id) => {
-  try {
     const response = await fetch(
       `http://localhost:3000/user/${id}/performance`
     );
     const data = await response.json();
     return data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
-const getUserActivity = async (id) => {
-  try {
+const getUserActivity = async (id) => { 
     const response = await fetch(`http://localhost:3000/user/${id}/activity`);
     const data = await response.json();
     return data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 const getUserAverage = async (id) => {
-  try {
     const response = await fetch(`http://localhost:3000/user/${id}/average`);
     const data = await response.json();
     return data;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 export { getUserInfo, getUserPerformance, getUserActivity, getUserAverage };
