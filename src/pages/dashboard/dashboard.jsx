8590cca4d05636844200581f8df3f4c2bee66eb9
Olivia Gautheron <oxtramag@hotmail.fr>
2022-09-03 11:28:02 +0200

    WIP

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./dashboard.css";
-import mockData from "../../services/mockData";
 import KeyFigures from "../../components/keyFigures/keyFigures";
 import {
   getUserInfo,
@@ -15,7 +14,6 @@ import TypeOfPerformance from "../../components/typeOfPerformance/typeOfPerforma
 import AverageScore from "../../components/averageScore/averageScore";
 
 const Dashboard = () => {
-  const [isMockData, setIsMockData] = useState(true);
   const [dataUser, setDataUSer] = useState([]);
   const [dataActivity, setDataActivity] = useState([]);
   const [dataSession, setDataSession] = useState([]);
@@ -24,43 +22,28 @@ const Dashboard = () => {
   let { id } = useParams();
   let userId = parseInt(id);
 
-  const userInfo = mockData.USER_MAIN_DATA.find((item) => item.id === userId);
-  const userActivity = mockData.USER_ACTIVITY.find(
-    (item) => item.userId === userId
-  );
-  const userSession = mockData.USER_AVERAGE_SESSIONS.find(
-    (item) => item.userId === userId
-  );
-  const userPerformance = mockData.USER_PERFORMANCE.find(
-    (item) => item.userId === userId
-  );
-
   useEffect(() => {
     const getDataUser = async () => {
       const request = await getUserInfo(id);
       setDataUSer(request.data);
-      setIsMockData(false);
     };
     getDataUser();
 
     const getDataActivity = async () => {
       const request = await getUserActivity(userId);
       setDataActivity(request.data);
-      setIsMockData(false);
     };
     getDataActivity();
 
     const getDataSession = async () => {
       const request = await getUserAverage(userId);
       setDataSession(request.data);
-      setIsMockData(false);
     };
     getDataSession();
 
     const getDataPerformance = async () => {
       const request = await getUserPerformance(userId);
       setDataPerformance(request.data);
-      setIsMockData(false);
     };
     getDataPerformance();
   }, [id, userId]);
@@ -72,56 +55,34 @@ const Dashboard = () => {
           <h1 className="header__title">
             Bonjour {""}
             <span className="header__title--color">
-              {isMockData
-                ? userInfo.userInfos.firstName
-                : dataUser.userInfos.firstName}
+              {dataUser.userInfos?.firstName}
             </span>
           </h1>
           <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
         </div>
         <section className="dashboard_stat">
           <article className="stat__dailyActivity">
-            {isMockData ? (
-              <DailyActivity activityData={userActivity.sessions} />
-            ) : (
-              <DailyActivity activityData={dataActivity.sessions} />
-            )}
+            <DailyActivity activityData={dataActivity.sessions} />
           </article>
 
           <article className="stat__charts">
             <div className="charts__sessionDuration">
               <div className="charts__sessionDuration__background"></div>
-              {isMockData ? (
-                <SessionDuration averageData={userSession.sessions} />
-              ) : (
-                <SessionDuration averageData={dataSession.sessions} />
-              )}
+              <SessionDuration averageData={dataSession.sessions} />
             </div>
 
             <div className="charts__typeOfPerformance">
-              {isMockData ? (
-                <TypeOfPerformance performanceData={userPerformance.data} />
-              ) : (
-                <TypeOfPerformance performanceData={dataPerformance.data} />
-              )}
+              <TypeOfPerformance performanceData={dataPerformance.data} />
             </div>
 
             <div className="charts__averageScore">
-              {isMockData ? (
-                <AverageScore scorePercent={userInfo.todayScore} />
-              ) : (
-                <AverageScore scorePercent={dataUser.todayScore} />
-              )}
+              <AverageScore scorePercent={dataUser.todayScore} />
             </div>
           </article>
         </section>
 
         <section className="dashboard__keyFigures">
-          {isMockData ? (
-            <KeyFigures figureData={userInfo.keyData} />
-          ) : (
-            <KeyFigures figureData={dataUser.keyData} />
-          )}
+          <KeyFigures figureData={dataUser.keyData} />
         </section>
       </div>
     </>
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,48 +1,67 @@
-//let isDataApi = false
+import mockData from "./mockData";
+
+const mockedEnv = false;
+//process.env.REACT_APP_IS_DATA_MOCK;
 
 const getUserInfo = async (id) => {
-  try {
-    const response = await fetch(`http://localhost:3000/user/${id}`);
-    const data = await response.json();
-    if (data.data.todayScore === undefined)
-      data.data.todayScore = data.data.score;
-    return data;
-  } catch (error) {
-    console.log(error);
+  if (mockedEnv) {
+    return mockData.USER_MAIN_DATA.find((item) => item.userId === id);
+  } else {
+    try {
+      const response = await fetch(`http://localhost:3000/user/${id}`);
+      const data = await response.json();
+      if (data.data.todayScore === undefined)
+        data.data.todayScore = data.data.score;
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
 const getUserPerformance = async (id) => {
-  try {
-    const response = await fetch(
-      `http://localhost:3000/user/${id}/performance`
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+  if (mockedEnv) {
+    return mockData.USER_PERFORMANCE.find((item) => item.userId === id);
+  } else {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/user/${id}/performance`
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
 const getUserActivity = async (id) => {
-  try {
-    const response = await fetch(`http://localhost:3000/user/${id}/activity`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+  if (mockedEnv) {
+    return mockData.USER_ACTIVITY.find((item) => item.userId === id);
+  } else {
+    try {
+      const response = await fetch(`http://localhost:3000/user/${id}/activity`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
 const getUserAverage = async (id) => {
-  try {
-    const response = await fetch(
-      `http://localhost:3000/user/${id}/average-sessions`
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+  if (mockedEnv) {
+    return mockData.USER_AVERAGE_SESSIONS.find((item) => item.userId === id);
+  } else {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/user/${id}/average-sessions`
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
